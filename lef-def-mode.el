c661fc1576b249222ccca3e683d581e1ea95c27d
Aritra-b-11 <aritrabhattacharjee12@gmail.com>
2017-05-13 12:34:13 +0530

    added basic things

--- lef-def-mode.el
+++ lef-def-mode.el
@@ -14,7 +14,7 @@
 (defvar lef-def-mode-hook nil "This is the mode hook for LEF-DEF-MODE for Emacs.")
 
 (defvar lef-def-mode-keymap
-  (let ((map make-sparse-keymap))
+  (let ((map (make-sparse-keymap)))
     (define-key map "\C-j" 'newline-and-indent)
     map)
   "Keymap for LEF-DEF major mode.")
@@ -22,5 +22,22 @@
 ;;; ### autoload
 (add-to-list 'auto-mode-alist '("\\.[ld]ef$" . lef-def-mode))
 
-(provide 'lef-def-mode-hook)
+(defconst lef-def-font-lock-keyword-face-1
+  (list
+   '("\\<\\(#.*$\\)\\>" . font-lock-comment-face))
+  "Highliting for LEF-DEF-MODE.")
+
+
+(defvar lef-def-mode-font-lock-keywords lef-def-font-lock-keyword-face-1
+  "Keywords for LEF DEF font lock.")
+
+;; make the LEF DEF MODE from here
+;; add the keymap & syntax hilight after doc string
+(define-derived-mode lef-def-mode fundamental-mode "LEF DEF Mode"
+  "This is a Fundamental mode for editing LEF DEF files."
+  (set (make-local-variable 'font-lock-defaults) '(lef-def-font-lock-keywords))
+  )
+
+(provide 'lef-def-mode)
+
 ;;; lef-def-mode.el ends here
